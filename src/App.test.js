88797475d4487327c28baf5a4f4b06d8308bc839
indu soni <indu.s@peopleinteractive.in>
2019-02-01 11:37:47 +0530

    config chenges

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
+  const div = document.createElement('h1');
   ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.unmountComponentAtNode('h1');
 });
